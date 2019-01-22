8a1278b64c437c812826fa8d64ff5bb47988c05d
ktosox <k>
2019-01-22 22:29:42 +0100

    dabu

--- Scenes_N_Scripts/ItemSlot.gd
+++ Scenes_N_Scripts/ItemSlot.gd
@@ -10,12 +10,12 @@ func _ready():
 	pass
 
 func get_drag_data(position):
+	#
 	#print("drag started")
 	var moving = ColorRect.new()
 	moving.rect_size = Vector2(rect_size.x,rect_size.y)
 	moving.color=color
 	set_drag_preview(moving)
-	$AudioGrab.play()
 	return ItemData
 
 func can_drop_data(position, data):
@@ -27,20 +27,32 @@ func drop_data(position, newItem):
 	Inventory.swapSlots(ItemData["ID"],newItem["ID"])
 	#$AudioDrop.play()#sholud be on magaer level
 
-func updateColor(newColor):
-	ItemData["ItemType"] = newColor
-	color = newColor
-
-func getItemTexture():
+func updateItemTexture():
 	#request texture from inventory
 	#$HeldItemTexture.texture =
 	pass
+	
 func freeItem():
 	#remove all item data
+	ItemData["ID"] = 0
+	ItemData["ItemType"] = "null"
+	ItemData["ItemName"] = "null"
 	#make empty
 	pass
 
 func changeItem(newItemData):
 	#update the ItemData held by this ItemSlot
 	#reload HeldITemTexture
+	pass
+	
+func lockSlot(type = 0):
+	#locks down the given item slot
+	#defualts to 0 which is gray out
+	#gray out would be $Overlay.color = Color(0.8,0.8,0.8,0.4) or something
+	#other types of locks can be implemented later
+	IsLocked = true
+	pass
+func unlockSlot():
+	# $Overlay.a = 0 i guess?
+	IsLocked = false
 	pass
