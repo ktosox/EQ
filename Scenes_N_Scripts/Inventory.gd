14e3178203de87f394a48389759395c0f2dbcec1
Unknown <ktoso3@gmail.com>
2019-01-27 13:06:37 +0100

    project description added - WIP

--- Scenes_N_Scripts/Inventory.gd
+++ Scenes_N_Scripts/Inventory.gd
@@ -5,20 +5,20 @@ var parentManager #this holds the manager controling this inventory
 
 
 #DEFINE NUMBER OF ITEM SLOTS
-const numberOfSlotsX = 5
-const numberOfSlotsY = 5
+#const numberOfSlotsX = 5
+#const numberOfSlotsY = 5
 
 #DEFINE SIZE/SHAPE OF ITEM SLOTS
-const sizeOfItemSlotX = 35
-const sizeOfItemSlotY = 35
+#const sizeOfItemSlotX = 35
+#const sizeOfItemSlotY = 35
 
 #DEFINE HOW BIG THE BOARDER SHOULD BE
-const sizeOfInventoryBoarderX = 15
-const sizeOfInventoryBoarderY = 15
+#const sizeOfInventoryBoarderX = 15
+#const sizeOfInventoryBoarderY = 15
 
 #DEFINE SPACE BETWEEN SLOTS
-const sizeOfItemSlotGapX = 0
-const sizeOfItemSlotGapY = 0
+#const sizeOfItemSlotGapX = 0
+#const sizeOfItemSlotGapY = 0
 
 #preload item class
 var ItemSlotBase = load("res://Scenes_N_Scripts/ItemSlot.tscn")
@@ -28,13 +28,13 @@ var ListOfItemSlots = {}
 
 
 func _ready():
-	#rescale this so it can hold all of the slots
-	var InventorySizeX = numberOfSlotsX * sizeOfItemSlotX + 2 * sizeOfInventoryBoarderX + (numberOfSlotsX-1) * sizeOfItemSlotGapX
-	var InventorySizeY = numberOfSlotsY * sizeOfItemSlotY + 2 * sizeOfInventoryBoarderY + (numberOfSlotsY-1) * sizeOfItemSlotGapY
+		#rescale this so it can hold all of the slots
+	#var InventorySizeX = numberOfSlotsX * sizeOfItemSlotX + 2 * sizeOfInventoryBoarderX + (numberOfSlotsX-1) * sizeOfItemSlotGapX
+	#var InventorySizeY = numberOfSlotsY * sizeOfItemSlotY + 2 * sizeOfInventoryBoarderY + (numberOfSlotsY-1) * sizeOfItemSlotGapY
 	self.rect_size = Vector2(InventorySizeX,InventorySizeY)
-	#move the inner slot container so its centerd in the Inventory
-	$ItemSlotLayer.rect_global_position.x = self.rect_global_position.x + sizeOfInventoryBoarderX
-	$ItemSlotLayer.rect_global_position.y = self.rect_global_position.y + sizeOfInventoryBoarderY
+		#move the inner slot container so its centerd in the Inventory
+	#$ItemSlotLayer.rect_global_position.x = self.rect_global_position.x + sizeOfInventoryBoarderX
+	#$ItemSlotLayer.rect_global_position.y = self.rect_global_position.y + sizeOfInventoryBoarderY
 	#fillItemSlotGrid() #place all of the itemslots
 
 func fillItemSlotGrid():
